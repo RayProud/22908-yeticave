7c8a773b04c17c87c33996edf00dc259813622f2
Прудников Роман Алексеевич <r.prudnikov@2gis.ru>
2019-02-04 11:05:47 +0300

    Change prices to float

--- index.php
+++ index.php
@@ -4,23 +4,23 @@ $is_auth = rand(0, 1);
 $user_name = 'Роман Прудников';
 $categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 $lots = [
-    ['title' => '2014 Rossignol District Snowboard', 'category' => 'Доски и лыжи', 'price' => 10999, 'image' => 'img/lot-1.jpg'],
-    ['title' => 'DC Ply Mens 2016/2017 Snowboard', 'category' => 'Доски и лыжи', 'price' => 159999, 'image' => 'img/lot-2.jpg'],
+    ['title' => '2014 Rossignol District Snowboard', 'category' => 'Доски и лыжи', 'price' => 10999.2, 'image' => 'img/lot-1.jpg'],
+    ['title' => 'DC Ply Mens 2016/2017 Snowboard', 'category' => 'Доски и лыжи', 'price' => 159999.99, 'image' => 'img/lot-2.jpg'],
     ['title' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'category' => 'Крепления', 'price' => 8000, 'image' => 'img/lot-3.jpg'],
-    ['title' => 'Ботинки для сноуборда DC Mutiny Charocal', 'category' => 'Ботинки', 'price' => 10999, 'image' => 'img/lot-4.jpg'],
-    ['title' => 'Куртка для сноуборда DC Mutiny Charocal', 'category' => 'Одежда', 'price' => 7500, 'image' => 'img/lot-5.jpg'],
-    ['title' => 'Маска Oakley Canopy', 'category' => 'Разное', 'price' => 5400, 'image' => 'img/lot-6.jpg']
+    ['title' => 'Ботинки для сноуборда DC Mutiny Charocal', 'category' => 'Ботинки', 'price' => 10999.0, 'image' => 'img/lot-4.jpg'],
+    ['title' => 'Куртка для сноуборда DC Mutiny Charocal', 'category' => 'Одежда', 'price' => 7500.0, 'image' => 'img/lot-5.jpg'],
+    ['title' => 'Маска Oakley Canopy', 'category' => 'Разное', 'price' => 5400.0, 'image' => 'img/lot-6.jpg']
 ];
 
 /**
  * Format a price in Russian notation and append a RUR symbol
  *
- * @param int $price
+ * @param float $price
  *
  * @return string
  */
-function format_price(int $price): string {
-    $formatted_price = number_format($price, 0, '.', ' ');
+function format_price(float $price): string {
+    $formatted_price = number_format(ceil($price), 0, '.', ' ');
 
     return $formatted_price . ' <b class="rub">р</b>';
 }
