4cae1f7d883a850d87a1bd9561c71e765db8d449
Roman Prudnikov <stillerr@ya.ru>
2019-02-09 15:00:47 +0300

    Add indexes

--- schema.sql
+++ schema.sql
@@ -5,39 +5,55 @@ CREATE DATABASE yeticave
 USE yeticave;
 
 CREATE TABLE category (
-  category_id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title CHAR(32) NOT NULL UNIQUE
 );
 
+CREATE UNIQUE INDEX category_id on category(id);
+CREATE INDEX category_title on category(title);
+
+CREATE TABLE user (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  registered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  email CHAR(255) UNIQUE,
+  name CHAR(255) NOT NULL,
+  password CHAR(32) NOT NULL,
+  image CHAR(255),
+  contacts TEXT(1024)
+);
+
+CREATE UNIQUE INDEX user_id on user(id);
+CREATE INDEX user_name on user(name);
+
 CREATE TABLE lot (
-  lot_id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   title CHAR(128) NOT NULL,
   description TEXT(1024),
   image CHAR(255),
   start_price DECIMAL NOT NULL,
-  end_at TIMESTAMP NOT NULL,
+  end_at DATETIME,
   bet_step DECIMAL NOT NULL,
   author_id INT NOT NULL,
   winner_id INT,
-  category_id INT NOT NULL
+  category_id INT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user(id),
+  FOREIGN KEY (winner_id) REFERENCES user(id),
+  FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
+CREATE UNIQUE INDEX lot_id on lot(id);
+CREATE INDEX lot_title on lot(title);
+
 CREATE TABLE bet (
-  bet_id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   amount DECIMAL NOT NULL,
   author_id INT NOT NULL,
-  lot_id INT NOT NULL
+  lot_id INT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user(id),
+  FOREIGN KEY (lot_id) REFERENCES lot(id)
 );
 
-CREATE TABLE user (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  registered_at TIMESTAMP,
-  email CHAR(255) UNIQUE,
-  name CHAR(255),
-  password CHAR(32),
-  image CHAR(255),
-  contacts TEXT(1024),
-  lots CHAR(255)
-);
+CREATE UNIQUE INDEX bet_id on bet(id);
+
